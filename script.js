e30790a1ac4b83e318a79397f988ec16bc22ae30
Ariel Santos <aricvs@gmail.com>
2023-11-21 14:34:53 -0300

    Set up display gameboard function

--- script.js
+++ script.js
@@ -6,4 +6,14 @@ const gameboard = {
   board: [1, 2, 3, 4, 5, 6, 7, 8, 9],
 };
 
+function displayGameboard() {
+  console.log(
+    `${gameboard.board[0]} | ${gameboard.board[1]} | ${gameboard.board[2]}\n---------
+${gameboard.board[3]} | ${gameboard.board[4]} | ${gameboard.board[5]}\n---------
+${gameboard.board[6]} | ${gameboard.board[7]} | ${gameboard.board[8]}`
+  );
+}
+
+displayGameboard();
+
 // })();
