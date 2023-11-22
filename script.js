36eea4db41148ca6164ea80b43c745ad2ff50c65
Ariel Santos <aricvs@gmail.com>
2023-11-22 16:36:54 -0300

    Add function to fill cell with mark

--- script.js
+++ script.js
@@ -11,6 +11,35 @@ const Gameboard = (function () {
   return { board, displayBoard };
 })();
 
-function CreatePlayer(mark) {
+function Player(mark) {
   return { mark };
 }
+
+let gameboard = Gameboard;
+let player1 = Player("X");
+let player2 = Player("O");
+let currentPlayer = 1;
+let gameOver = false;
+
+const makePick = function () {
+  const cells = document.querySelectorAll(".container__cells");
+
+  cells.forEach((cell) => {
+    cell.addEventListener("click", () => {
+      if (currentPlayer === 1) {
+        gameboard.board[cell.getAttribute("cellIdx")] = player1.mark;
+        switchPlayer();
+      } else {
+        gameboard.board[cell.getAttribute("cellIdx")] = player2.mark;
+        switchPlayer();
+      }
+      gameboard.displayBoard();
+    });
+  });
+};
+
+const switchPlayer = function () {
+  currentPlayer === 1 ? (currentPlayer = 2) : (currentPlayer = 1);
+};
+
+makePick();
