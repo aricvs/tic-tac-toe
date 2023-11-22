bf4751d8de293e281d6388d671601361c8051f4c
Ariel Santos <aricvs@gmail.com>
2023-11-22 11:24:33 -0300

    Implement win condition

--- script.js
+++ script.js
@@ -52,7 +52,33 @@ function playRound() {
 // TODO: implement main game function, a while loop to play rounds and check
 // for win condition after each round
 
-// TODO: implement a way to check for win conditions
-
+// check win conditions and return the winning player
+function checkWin() {
+  let board = gameboard.board;
+  if (
+    (board[0] === "X" && board[1] === "X" && board[2] === "X") ||
+    (board[3] === "X" && board[4] === "X" && board[5] === "X") ||
+    (board[6] === "X" && board[7] === "X" && board[8] === "X") ||
+    (board[0] === "X" && board[3] === "X" && board[6] === "X") ||
+    (board[1] === "X" && board[4] === "X" && board[7] === "X") ||
+    (board[2] === "X" && board[5] === "X" && board[8] === "X") ||
+    (board[0] === "X" && board[4] === "X" && board[8] === "X") ||
+    (board[2] === "X" && board[4] === "X" && board[6] === "X")
+  ) {
+    return 1;
+  }
+  if (
+    (board[0] === "O" && board[1] === "O" && board[2] === "O") ||
+    (board[3] === "O" && board[4] === "O" && board[5] === "O") ||
+    (board[6] === "O" && board[7] === "O" && board[8] === "O") ||
+    (board[0] === "O" && board[3] === "O" && board[6] === "O") ||
+    (board[1] === "O" && board[4] === "O" && board[7] === "O") ||
+    (board[2] === "O" && board[5] === "O" && board[8] === "O") ||
+    (board[0] === "O" && board[4] === "O" && board[8] === "O") ||
+    (board[2] === "O" && board[4] === "O" && board[6] === "O")
+  ) {
+    return 2;
+  }
+}
 // uncomment this once the core code is complete
 // })();
