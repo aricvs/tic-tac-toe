808748df8fec980c420cc6808b9b27db45a06998
Ariel Santos <aricvs@gmail.com>
2023-11-22 15:57:21 -0300

    Remove game module

--- script.js
+++ script.js
@@ -1,29 +1,8 @@
 "use strict";
 
-const game = {
-  turn: 1,
-  player1: Players("X"),
-  player2: Players("O"),
-
-  makePick: function () {
-    let currentPlayer;
-    if (this.turn === 1) {
-      currentPlayer = this.player1;
-      this.switchTurn();
-    } else {
-      currentPlayer = this.player2;
-      this.switchTurn();
-    }
-  },
-
-  switchTurn: function () {
-    this.turn === 1 ? (this.turn = 2) : (this.turn = 1);
-  },
-};
-
 const Gameboard = (function () {
   const board = ["", "", "", "", "", "", "", "", ""];
-  const cells = document.querySelectorAll(".container__square");
+  const cells = document.querySelectorAll(".container__cells");
   function displayBoard() {
     cells.forEach(
       (cell) => (cell.textContent = this.board[cell.getAttribute("cellIdx")])
@@ -32,8 +11,6 @@ const Gameboard = (function () {
   return { board, displayBoard };
 })();
 
-function Players(mark) {
+function CreatePlayer(mark) {
   return { mark };
 }
-
-Gameboard.displayBoard();
