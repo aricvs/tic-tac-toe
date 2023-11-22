af7d584e5e4bcea0901f788b7bd260640e930a12
Ariel Santos <aricvs@gmail.com>
2023-11-22 20:42:00 -0300

    Modularize game function

--- script.js
+++ script.js
@@ -14,33 +14,34 @@ function Player(mark) {
   return { mark };
 }
 
-// TODO: put below in an IIFE called "game"
-const gameMsg = document.querySelector(".container__messages");
-const cells = document.querySelectorAll(".container__cells");
-const gameboard = Gameboard;
-const winConditions = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-const player1 = Player("X");
-const player2 = Player("O");
-let currentPlayer = 1;
-let running = false;
+const Game = (function () {
+  const gameMsg = document.querySelector(".container__messages");
+  const cells = document.querySelectorAll(".container__cells");
+  const gameboard = Gameboard;
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  const player1 = Player("X");
+  const player2 = Player("O");
+  let currentPlayer = 1;
+  let running = false;
 
-const startGame = function () {
-  cells.forEach((cell) => cell.addEventListener("click", clickCell));
-};
+  const startGame = function () {
+    cells.forEach((cell) => cell.addEventListener("click", clickCell));
+  };
 
-const clickCell = function () {};
+  const clickCell = function () {};
 
-const fillCell = function (cell, index) {};
+  const fillCell = function (cell, index) {};
 
-const switchPlayer = function () {};
+  const switchPlayer = function () {};
 
-const checkWin = function () {};
+  const checkWin = function () {};
+})();
