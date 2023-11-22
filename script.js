900b1502427ce9205b73f075bee863a34664063a
Ariel Santos <aricvs@gmail.com>
2023-11-22 20:07:36 -0300

    Add check win function

--- script.js
+++ script.js
@@ -45,4 +45,32 @@ const switchPlayer = function () {
   currentPlayer === 1 ? (currentPlayer = 2) : (currentPlayer = 1);
 };
 
+const checkWin = function () {
+  const bd = gameboard.board;
+
+  if (
+    (bd[0] === "X" && bd[1] === "X" && bd[2] === "X") ||
+    (bd[3] === "X" && bd[4] === "X" && bd[5] === "X") ||
+    (bd[6] === "X" && bd[7] === "X" && bd[8] === "X") ||
+    (bd[0] === "X" && bd[3] === "X" && bd[6] === "X") ||
+    (bd[1] === "X" && bd[4] === "X" && bd[7] === "X") ||
+    (bd[2] === "X" && bd[5] === "X" && bd[2] === "X") ||
+    (bd[0] === "X" && bd[4] === "X" && bd[8] === "X") ||
+    (bd[2] === "X" && bd[4] === "X" && bd[6] === "X")
+  ) {
+    return 1;
+  } else if (
+    (bd[0] === "O" && bd[1] === "O" && bd[2] === "O") ||
+    (bd[3] === "O" && bd[4] === "O" && bd[5] === "O") ||
+    (bd[6] === "O" && bd[7] === "O" && bd[8] === "O") ||
+    (bd[0] === "O" && bd[3] === "O" && bd[6] === "O") ||
+    (bd[1] === "O" && bd[4] === "O" && bd[7] === "O") ||
+    (bd[2] === "O" && bd[5] === "O" && bd[2] === "O") ||
+    (bd[0] === "O" && bd[4] === "O" && bd[8] === "O") ||
+    (bd[2] === "O" && bd[4] === "O" && bd[6] === "O")
+  ) {
+    return 2;
+  }
+};
+
 makePick();
