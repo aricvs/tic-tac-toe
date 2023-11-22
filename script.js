919106172d78aaa8920061a3fcbccd3063aaed96
Ariel Santos <aricvs@gmail.com>
2023-11-22 15:17:07 -0300

    Add functions to detect player turn and switch

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 "use strict";
+
 const game = {
   turn: 1,
+  player1: Players("X"),
+  player2: Players("O"),
+
+  makePick: function () {
+    let currentPlayer;
+    if (this.turn === 1) {
+      currentPlayer = this.player1;
+      this.switchTurn();
+    } else {
+      currentPlayer = this.player2;
+      this.switchTurn();
+    }
+  },
+
+  switchTurn: function () {
+    this.turn === 1 ? (this.turn = 2) : (this.turn = 1);
+  },
 };
 
 const Gameboard = (function () {
   const board = ["", "", "", "", "", "", "", "", ""];
+  const cells = document.querySelectorAll(".container__square");
   function displayBoard() {
-    const cells = document.querySelectorAll(".container__square");
     cells.forEach(
       (cell) => (cell.textContent = this.board[cell.getAttribute("cellIdx")])
     );
